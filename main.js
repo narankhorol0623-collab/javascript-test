1b054e481acdf2d9b95bc031e401df109a9882eb
25LP1579 25LP1579 <25LP1579@G304-CC0223-2.local>
2025-10-27 12:47:56 +0800

    yuch bolood bgayn

---
+++ main.js
@@ -0,0 +1,72 @@
+// let hour= 2.5;
+// console.log(hour * 60);
+// let hour1 = 3;
+// console.log(hour1 *60)
+
+// const num= 5;
+// console.log(num %2 ===1 && "odd")
+// console.log(num %2 ===0 && "even")
+
+// const num2= 6;
+// console.log(num2 %2 ===1 && "odd")
+// console.log(num2 %2 ===0 && "even")
+
+// let a = 2,
+//   b = 7,
+//   c = 8;
+// console.log(a < b && b < c && "true");
+
+// let f = 9,
+//   d = 22,
+//   e = 7;
+
+// console.log(f < d && d < e && e);
+// console.log(d < e && e < f && f);
+// console.log(e < f && f < d && d);
+
+// let etc = v=4, l=7, p=5;
+
+// console.log( l > p && p > v && l,p,v );
+// console.log( p > l && l > v && p,l,v);
+// console.log( p > l && l > v && p,l,v);
+
+const a = 3,
+  b = 3,
+  c = 6;
+console.log(a + b + c);
+
+let d = 1,
+  e = 3,
+  f = 5,
+  g = 7,
+  h = 8,
+  i = 8.7;
+console.log(d + e + f + g + h + (i - 0.7));
+
+let urt = 10,
+  urgun = 5;
+console.log(urt * urgun);
+console.log(urt * 2 + urgun * 2);
+
+const seconds = 138;
+const minutes = parseInt(seconds / 60);
+const second = seconds % 60;
+
+console.log(minutes, "min", second, "seconds");
+
+let seconds2 = 1297;
+
+let hour2 = parseInt(seconds2 / 3600)
+let minutes2 = parseInt(seconds2 / 60);
+let seconds3 = seconds2 % 60;
+console.log(hour2, "h", minutes2, "min", seconds3, "seconds");
+
+
+const m = 875,
+  s = 88;
+console.log(m * 60 + 88);
+
+let hour = 83,
+  minut1 = 84,
+  sk = 85;
+console.log(hour * 60 * 60 + 84 * 60 + 85);
