28d6c250fefc9c4c1069ece0be9b69090831c596
25LP1579 25LP1579 <25LP1579@G304-CC0223-2.local>
2025-10-29 12:26:05 +0800

    teeestiinnnggg

--- main.js
+++ main.js
@@ -61,145 +61,145 @@
 // let seconds3 = seconds2 % 60;
 // console.log(hour2, "h", minutes2, "min", seconds3, "seconds");
 
-let honog = 38;
-let honog1 = parseInt(honog / 24);
-let tsag = parseInt(honog % 24);
-console.log(honog1, "day", tsag, "hour");
+// let honog = 38;
+// let honog1 = parseInt(honog / 24);
+// let tsag = parseInt(honog % 24);
+// console.log(honog1, "day", tsag, "hour");
 
-let day = 4;
-tsg = 40;
-console.log(day * 24 + 40, "hour");
+// let day = 4;
+// tsg = 40;
+// console.log(day * 24 + 40, "hour");
 
-let month = 106;
-let jil = parseInt(month / 12);
-let sar = parseInt(month % 12);
-console.log(jil, "year", sar, "month");
+// let month = 106;
+// let jil = parseInt(month / 12);
+// let sar = parseInt(month % 12);
+// console.log(jil, "year", sar, "month");
 
-let jil2 = 90;
-sar2 = 98;
-console.log(jil2 * 12 + 98, "month");
+// let jil2 = 90;
+// sar2 = 98;
+// console.log(jil2 * 12 + 98, "month");
 
-let numb1 = 2,
-  numb2 = 4,
-  numb3 = 7,
-  numb4 = 5;
+// let numb1 = 2,
+//   numb2 = 4,
+//   numb3 = 7,
+//   numb4 = 5;
 
-let min = numb1;
-if (min > numb1) {
-  min = numb1;
-}
+// let min = numb1;
+// if (min > numb1) {
+//   min = numb1;
+// }
 
-if (min > numb2) {
-  min = numb2;
-}
+// if (min > numb2) {
+//   min = numb2;
+// }
 
-if (min > numb3) {
-  min = numb3;
-}
+// if (min > numb3) {
+//   min = numb3;
+// }
 
-if (min > numb4) {
-  min = numb4;
-}
-console.log(min);
+// if (min > numb4) {
+//   min = numb4;
+// }
+// console.log(min);
 
-let too1 = 2,
-  too2 = 9,
-  too3 = 4,
-  too4 = 3;
-let too5 = 1;
+// let too1 = 2,
+//   too2 = 9,
+//   too3 = 4,
+//   too4 = 3;
+// let too5 = 1;
 
-if (too1 < 5) {
-  too5 = too5 * too1;
-}
-if (too2 < 5) {
-  too5 = too5 * too2;
-}
-if (too3 < 5) {
-  too5 = too5 * too3;
-}
-if (too4 < 5) {
-  too5 = too5 * too4;
-}
-console.log(too5);
+// if (too1 < 5) {
+//   too5 = too5 * too1;
+// }
+// if (too2 < 5) {
+//   too5 = too5 * too2;
+// }
+// if (too3 < 5) {
+//   too5 = too5 * too3;
+// }
+// if (too4 < 5) {
+//   too5 = too5 * too4;
+// }
+// console.log(too5);
 
-let a = 2,
-  b = 9,
-  c = 10,
-  d = 4,
-  e = 7,
-  f = 6;
-let hehe = 1;
+// let a = 2,
+//   b = 9,
+//   c = 10,
+//   d = 4,
+//   e = 7,
+//   f = 6;
+// let hehe = 1;
 
-if (a > 5) {
-  hehe = hehe * a;
-}
+// if (a > 5) {
+//   hehe = hehe * a;
+// }
 
-if (b > 5) {
-  hehe = hehe * b;
-}
+// if (b > 5) {
+//   hehe = hehe * b;
+// }
 
-if (c > 5) {
-  hehe = hehe * c;
-}
+// if (c > 5) {
+//   hehe = hehe * c;
+// }
 
-if (d > 5) {
-  hehe = hehe * d;
-}
-if (e > 5) {
-  hehe = hehe * e;
-}
-if (f > 5) {
-  hehe = hehe * f;
-}
+// if (d > 5) {
+//   hehe = hehe * d;
+// }
+// if (e > 5) {
+//   hehe = hehe * e;
+// }
+// if (f > 5) {
+//   hehe = hehe * f;
+// }
 
-console.log(hehe);
+// console.log(hehe);
 
-const ye1 = 178787287;
-const ye2 = 676736726;
-if (ye1 > ye2) {
-  console.log(ye1);
-}
-if (ye1 < ye2) {
-  console.log(ye2);
-}
+// const ye1 = 178787287;
+// const ye2 = 676736726;
+// if (ye1 > ye2) {
+//   console.log(ye1);
+// }
+// if (ye1 < ye2) {
+//   console.log(ye2);
+// }
 
-let yeo1 = 6;
-if (yeo1 % 2 === 0) {
-  console.log("even");
-} else {
-  console.log("odd");
-}
+// let yeo1 = 6;
+// if (yeo1 % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
 
-let nara = 5,
-  sara = 6,
-  navcha = 8;
-let tsetsge = 6;
-if (tsetsge > nara) {
-  console.log(nara);
-}
-if (tsetsge > sara) {
-  console.log(sara);
-}
-if (tsetsge > navcha) {
-  console.log(navcha);
-}
-if (tsetsge < nara) {
-  console.log(nara);
-}
-if (tsetsge < sara) {
-  console.log(sara);
-}
-if (tsetsge < navcha) {
-  console.log(navcha);
-}
+// let nara = 5,
+//   sara = 6,
+//   navcha = 8;
+// let tsetsge = 6;
+// if (tsetsge > nara) {
+//   console.log(nara);
+// }
+// if (tsetsge > sara) {
+//   console.log(sara);
+// }
+// if (tsetsge > navcha) {
+//   console.log(navcha);
+// }
+// if (tsetsge < nara) {
+//   console.log(nara);
+// }
+// if (tsetsge < sara) {
+//   console.log(sara);
+// }
+// if (tsetsge < navcha) {
+//   console.log(navcha);
+// }
 
 // const hehh= "hgahghaghgahgahag";
 // console.log(hehh.length);
 
-// const bodlogo5 = "yuuugmeerchimde";
-// let urtn = bodlogo5.length;
-// let lastIndexofCharacter= urtn- 1
-// console.log(bodlogo5[0]+bodlogo5[lastIndexofCharacter])
+const bodlogo5 = "yuuugmeerchimde";
+let urtn = bodlogo5.length;
+let lastIndexofCharacter = urtn - 1;
+console.log(bodlogo5[0] + bodlogo5[lastIndexofCharacter]);
 
 // const bodlogo6 = "hi";
 // console.log(bodlogo6.toUpperCase());
@@ -208,57 +208,219 @@ if (tsetsge < navcha) {
 // console.log(bodlogo7 === 0);
 
 // bodlogo8
-const tooo1 = 81;
+// const tooo1 = 81;
 
-if (tooo1 % 15 === 0) {
-  console.log("FizzBuzz");
-} else if (tooo1 % 3 === 0) {
-  console.log("Fizz");
-} else if (tooo1 % 5 === 0) {
-  console.log("Buzz");
+// if (tooo1 % 15 === 0) {
+//   console.log("FizzBuzz");
+// } else if (tooo1 % 3 === 0) {
+//   console.log("Fizz");
+// } else if (tooo1 % 5 === 0) {
+//   console.log("Buzz");
+// }
+
+// const neg = "Hello";
+// const hoyr = "World";
+// console.log(neg, hoyr);
+
+// const let1 = 47,
+// let2 = 63;
+// console.log((let1+let2) /2);
+
+// const huu1 = -15;
+// if ( 0 > huu1){
+//   console.log("negative");
+// } else if ( 0 < huu1){
+//   console.log("plus");
+// } else if ( 0 === huu1) {
+//   console.log("zero");
+// }
+
+// const huu2 = 5;
+// if (huu2 > 10) {
+//   console.log("more than 10");
+// } else if (huu2 < 10) {
+//   console.log("less than 10");
+// }
+
+// const ok1 = 15, ok2 = 14;
+// if ( ok1 == ok2 ) {
+//   console.log("equal");
+// } else if ( ok1 != ok2 ) {
+//   console.log("diferrent");
+// }
+
+// const no1 = 105;
+// if ( no1 > 100 ) {
+//   console.log("+100");
+// } else if ( no1 < 100 ) {
+//   console.log("-100");
+// }
+
+// const nu1 = 100;
+// console.log(nu1 * 0.1, nu1 * 0.2, nu1 * 0.3);
+
+// let to1 = 25,
+// mu1 = 100;
+// if ( to1 >mu1 ){
+//   console.log(mu1);
+// } else if ( to1 < mu1 ){
+//   console.log(to1);
+// }
+
+// const temdeg1 = 700,
+// temdeg2 = 20;
+// console.log(temdeg1 === temdeg2);
+
+// let cifr1 = 78, cifr2 = 95;
+// let q = cifr1.length;
+// let s = cifr2.length;
+// console.log( q[0] + s[1] , q[1] + s[2] )
+
+// let ex1 = -216, ex2 = -1;
+// if ( ex1 < 0 ){
+//   console.log(ex1 * ex2);
+// } else if ( ex1 > 0 ){
+//   console.log( ex1 );
+// }
+
+// const mills = Date.now();
+//   const realsec = parseInt(mills / 1000);
+//   const realmin = parseInt(realsec / 60);
+//   const realhour = parseInt(realmin / 60);
+//   const realday = parseInt(realhour / 24);
+//   const realmonth = parseInt(realday / 30);
+//   const realyear = parseInt(realday / 365);
+
+//   const months = realmonth % 12;
+//   const days = realday % 365;
+//   const hours = (realhour % 24) + 8;
+//   const minute = realmin % 60;
+//   const second = realsec % 60;
+//   const millsecond = realmin % 1000;
+
+//   console.log(realyear ,"years", months, "months", days, "days", hours, "hours", minute, "minutes", second, "seconds", millsecond, "millseconds");
+
+const dugaar = 1371;
+
+if (((dugaar[3] = 1), (dugaar[3] = 6))) {
+  console.log("Monday");
+} else if (((dugaar[3] = 2), (dugaar[3] = 7))) {
+  console.log("Tuesday");
+}
+
+const i = 99,
+  you = 18,
+  we = 78;
+if (i < you && we < you) {
+  console.log(you);
+} else if (i > you && i > we) {
+  console.log(i);
+} else if (we > you && we > i) {
+  console.log(we);
+}
+
+const ishig = 70,
+  ymaa = 67,
+  uhana = 78;
+if (ishig < ymaa && ishig < uhana) {
+  console.log(ishig);
+}
+if (ymaa < ishig && ymaa < uhana) {
+  console.log(ymaa);
+}
+if (uhana < ishig && uhana < ymaa) {
+  console.log(uhana);
+}
+
+let dun1 = 75;
+
+if (dun1 >= 90 && dun1 <= 100) {
+  console.log(dun1, "A");
+} else if (dun1 <= 89 && dun1 >= 80) {
+  console.log(dun1, "B");
+} else if (dun1 <= 79 && dun1 >= 70) {
+  console.log(dun1, "C");
+} else if (dun1 <= 69 && dun1 >= 60) {
+  console.log(dun1, "D");
+} else if (dun1 <= 59 && dun1 >= 0) {
+  console.log(dun1, "F");
+}
+
+const urjver = 25;
+let yee = parseInt(urjver / 10);
+let yoo = urjver % 10;
+console.log(yee * yoo);
+
+
+const niilber = "116";
+const n = niilber.length;
+// const g = niilber[0];
+// const k = niilber[1];
+// const o = niilber[2];
+// console.log( Number[g] + Number[k] + Number[o]);
+console.log(n[0] + n[1] + n[2]);
+
+
+let jil3 = 3, sar3 = 7;
+let jilsar = jil3 * 12;
+console.log( jilsar + sar3 , "sar");
+
+
+let number1 = 56, number2 = 51, number3 = 48, number4 = 35;
+if (number1 > 50 && number2 > 50){
+  console.log(number1 * number2);
+}
+if (number2 > 50 && number3 > 50){
+  console.log(number2 * number3);
+}
+if (number3 > 50 && number4 > 50){
+  console.log(number3 * number4);
 }
 
 
 
-const neg = "Hello";
-const hoyr = "World";
-console.log(neg, hoyr);
-
-
-const let1 = 47,
-let2 = 63;
-console.log((let1+let2) /2);
-
-
-const huu1 = -15;
-if ( 0 > huu1){
-  console.log("negative");
-} else if ( 0 < huu1){
-  console.log("plus");
-} else if ( 0 === huu1) {
-  console.log("zero");
+let hii1 = 3, hii2 = 6, hii3 = 4;
+let niilber2 = 0;
+if (hii1 % 2 === 0){
+  niilber2 + hii1;
 }
-
-
-const huu2 = 5;
-if (huu2 > 10) {
-  console.log("more than 10");
-} else if (huu2 < 10) {
-  console.log("less than 10");
+else if (hii2 % 2 === 0){
+  niilber2 += hii2;
+} 
+else if (hii3 % 2 === 0){
+  niilber2 += hii3;
 }
+console.log(niilber2);
 
+  
+  
 
-const ok1 = 15, ok2 = 14;
-if ( ok1 == ok2 ) {
-  console.log("equal");
-} else if ( ok1 != ok2 ) {
-  console.log("diferrent");
+// let hoo1 = 5, hoo2 = 3, hoo3 = 3;
+// if (hii1 % 3 == 0){
+//   console.log(hoo1 * hoo2);
+// }
+// else if (hii2 % 3 == 0){
+//   console.log(hoo2 * hoo3);
+// } 
+// else if (hii3 % 3 == 0){
+//   console.log(hoo1 * hoo3);
+// }
+const a1=11;
+const a2=10;
+const a3=11;
+
+let urjver1=1;
+
+if(a1%2===1){
+  urjver1 *= a1
 }
-
-
-const no1 = 105;
-if ( no1 > 100 ) {
-  console.log("+100");
-} else if ( no1 < 100 ) {
-  console.log("-100");
+if(a2%2===1){
+  urjver1 *= a2
 }
+if(a3%2===1){
+  urjver1*=a3
+}
+console.log(urjver1);
+
+
+
