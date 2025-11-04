0e5dfc0399e699aad97e4ba00896742825456ca2
25LP1579 25LP1579 <25LP1579@G304-CC0223-2.local>
2025-11-04 12:28:09 +0800

    teeeestt

--- loop.js
+++ loop.js
@@ -1,327 +1,404 @@
-// // // console.log("Pinecone");
+// // // // console.log("Pinecone");
 
-// // // // for ( let i = 1 ; i <= 10 ; i ++) {
-// // // //     console.log("Pinecone" + i);
+// // // // // for ( let i = 1 ; i <= 10 ; i ++) {
+// // // // //     console.log("Pinecone" + i);
+// // // // // }
+
+// // // // // const n = 5;
+// // // // // for ( let i = 1 ; i <= n ; i ++) {
+// // // // //     console.log("PineCone" + i);
+// // // // // }
+
+// // // // // for ( let i = 1 ; i <= 20 ; i += 2) {
+// // // // //     console.log(i);
+// // // // // }
+
+// // // // // for ( let i = 0 ; i <= 40 ; i += 2) {
+// // // // //     console.log(i);
+// // // // // }
+
+// // // // // let niilber = 0;
+// // // // // for (let i = 0; i <= 20; i += 2) {
+// // // // //   niilber += i;
+// // // // // }
+// // // // // console.log(niilber);
+
+// // // // // let niilber2 = 0;
+// // // // // for (let i = 0; i <= 40; i += 2) {
+// // // // //   niilber2 += i;
+// // // // // }
+// // // // // console.log(niilber2);
+
+// // // // // let a1 = 0;
+// // // // // for (let i = 0; i <= 25; i++) {
+// // // // //   a1 += i;
+// // // // // }
+// // // // // console.log(a1);
+
+// // // // // const a2 = 100,
+// // // // //   n = 10;
+// // // // // let niilber3 = 0;
+// // // // // // console.log(a2 * n);
+// // // // // for (let i = 0; i < n; i++) {
+// // // // //   niilber3 += a2;
+// // // // // }
+// // // // // console.log(niilber3);
+
+// // // // // let urjver = 1;
+// // // // // for (let i = 1 ; i <= 5 ; i++) {
+// // // // //     urjver *= i ;
+// // // // // }
+// // // // // console.log(urjver);
+
+// // // // // let a4 = 1;
+// // // // // for (let i = 1 ; i <= 5 ; i += 2) {
+// // // // //     a4 *= i;
+// // // // // }
+// // // // // console.log(a4);
+
+// // // // // let n1 = 5 ;
+// // // // // let a5 = 1 ;
+// // // // // for ( let i = 1 ; i <= 5 ; i ++) {
+// // // // //     a5 *= i;
+// // // // // }
+// // // // // console.log(a5);
+
+// // // // // for ( let i = 10; i >= 0 ; i--){
+// // // // //     console.log(i);
+// // // // // }
+
+// // // // // const n2 = 2;
+// // // // // for ( let i = 1 ; i <= 10 ; i ++){
+// // // // //     console.log(`${n2}*${i}=${n2*i}`);
+// // // // // }
+
+// // // // // const num = 3, n3 = 4 ;
+// // // // // let zereg = 1 ;
+// // // // // for (let i = 0; i < n3; i++){
+// // // // //  zereg *= num
+// // // // // }
+// // // // // console.log(zereg);
+
+// // // // // const num2 = "325";
+// // // // // const urt1 = num2[0];
+// // // // // const urt2 = num2[1];
+// // // // // const urt3 = num2[2];
+
+// // // // // console.log(Number(urt1) + Number(urt2) + Number(urt3));
+
+// // // // // const num3 = "78373873837";
+// // // // // console.log(num3.length);
+
+// // // // // const kb = 5;
+// // // // // let urjver2 = 1 ;
+// // // // // for (let i = 0 ; i < 2; i++){
+// // // // //     urjver2 *= kb;
+// // // // // }
+// // // // // console.log(urjver2);
+
+// // // // // const n4 = 5 ;
+// // // // // let zereg1 = 1 ;
+// // // // // for (let i = 1; i <= n4; i ++){
+// // // // //     zereg1 *= i ;
+// // // // // }
+// // // // // console.log(zereg1);
+
+// // // // // const huvaagch = 2, huvaagch2 = 3;
+// // // // // for (let i = 1 ; i <= 30 ; i++){
+// // // // //     if(i % huvaagch === 0 && i % huvaagch2 === 0){
+// // // // //         console.log(i);
+// // // // //     }
+// // // // // }
+
+// // // // // const huvaagdagch1 = 12 , huvaagdagch2 = 15;
+// // // // // for ( let i = huvaagdagch1; i <= huvaagdagch2; i ++ ){
+// // // // //   if (i % 3 == 0){
+// // // // //     console.log("Fizz");
+// // // // //   } else if (i % 5 == 0){
+// // // // //     console.log("Buzz");
+// // // // //   }
+// // // // // }
+
+// // // // // let a6 = 1;
+// // // // // for ( let i = 1; i <= 100; i ++ ){
+// // // // //   if (i % 7 == 1){
+// // // // //     console.log(i);
+// // // // // }
+// // // // // }
+
+// // // // // for ( let i = 1; i <= 100; i++ ){
+// // // // //   if (i % 2 == 0 && i % 5 == 0){
+// // // // //     console.log(i);
+// // // // //   }
+// // // // // }
+
+// // // // for (let i = 1; i <= 10; i++) {
+// // // //   console.log(i * i);
 // // // // }
 
-// // // // const n = 5;
-// // // // for ( let i = 1 ; i <= n ; i ++) {
-// // // //     console.log("PineCone" + i);
-// // // // }
+// // // // const ner = "Horlo";
+// // // // console.log(ner[4] + ner[3] + ner[2] + ner[1] + ner[0]);
 
-// // // // for ( let i = 1 ; i <= 20 ; i += 2) {
-// // // //     console.log(i);
-// // // // }
+// // // // // let start = 1896, end = 2022;
+// // // // // for ( let i = start; i <= end; i++ ){
+// // // // //   if (i % 4 == 0){
+// // // // //     console.log("The olympic year");
 
-// // // // for ( let i = 0 ; i <= 40 ; i += 2) {
-// // // //     console.log(i);
-// // // // }
+// // // // //   } else if (i % 4 == 1){
+// // // // //     console.log("Not the olympic year");
 
-// // // // let niilber = 0;
-// // // // for (let i = 0; i <= 20; i += 2) {
-// // // //   niilber += i;
-// // // // }
-// // // // console.log(niilber);
+// // // // //   }
+// // // // // }
 
-// // // // let niilber2 = 0;
-// // // // for (let i = 0; i <= 40; i += 2) {
-// // // //   niilber2 += i;
-// // // // }
-// // // // console.log(niilber2);
-
-// // // // let a1 = 0;
-// // // // for (let i = 0; i <= 25; i++) {
-// // // //   a1 += i;
-// // // // }
-// // // // console.log(a1);
-
-// // // // const a2 = 100,
-// // // //   n = 10;
-// // // // let niilber3 = 0;
-// // // // // console.log(a2 * n);
-// // // // for (let i = 0; i < n; i++) {
-// // // //   niilber3 += a2;
-// // // // }
-// // // // console.log(niilber3);
-
-// // // // let urjver = 1;
-// // // // for (let i = 1 ; i <= 5 ; i++) {
-// // // //     urjver *= i ;
-// // // // }
-// // // // console.log(urjver);
-
-// // // // let a4 = 1;
-// // // // for (let i = 1 ; i <= 5 ; i += 2) {
-// // // //     a4 *= i;
-// // // // }
-// // // // console.log(a4);
-
-// // // // let n1 = 5 ;
-// // // // let a5 = 1 ;
-// // // // for ( let i = 1 ; i <= 5 ; i ++) {
-// // // //     a5 *= i;
-// // // // }
-// // // // console.log(a5);
-
-// // // // for ( let i = 10; i >= 0 ; i--){
-// // // //     console.log(i);
-// // // // }
-
-// // // // const n2 = 2;
-// // // // for ( let i = 1 ; i <= 10 ; i ++){
-// // // //     console.log(`${n2}*${i}=${n2*i}`);
-// // // // }
-
-// // // // const num = 3, n3 = 4 ;
-// // // // let zereg = 1 ;
-// // // // for (let i = 0; i < n3; i++){
-// // // //  zereg *= num
-// // // // }
-// // // // console.log(zereg);
-
-// // // // const num2 = "325";
-// // // // const urt1 = num2[0];
-// // // // const urt2 = num2[1];
-// // // // const urt3 = num2[2];
-
-// // // // console.log(Number(urt1) + Number(urt2) + Number(urt3));
-
-// // // // const num3 = "78373873837";
-// // // // console.log(num3.length);
-
-// // // // const kb = 5;
-// // // // let urjver2 = 1 ;
-// // // // for (let i = 0 ; i < 2; i++){
-// // // //     urjver2 *= kb;
-// // // // }
-// // // // console.log(urjver2);
-
-// // // // const n4 = 5 ;
-// // // // let zereg1 = 1 ;
-// // // // for (let i = 1; i <= n4; i ++){
-// // // //     zereg1 *= i ;
-// // // // }
-// // // // console.log(zereg1);
-
-// // // // const huvaagch = 2, huvaagch2 = 3;
-// // // // for (let i = 1 ; i <= 30 ; i++){
-// // // //     if(i % huvaagch === 0 && i % huvaagch2 === 0){
-// // // //         console.log(i);
-// // // //     }
-// // // // }
-
-// // // // const huvaagdagch1 = 12 , huvaagdagch2 = 15;
-// // // // for ( let i = huvaagdagch1; i <= huvaagdagch2; i ++ ){
-// // // //   if (i % 3 == 0){
-// // // //     console.log("Fizz");
-// // // //   } else if (i % 5 == 0){
-// // // //     console.log("Buzz");
-// // // //   }
-// // // // }
-
-// // // // let a6 = 1;
-// // // // for ( let i = 1; i <= 100; i ++ ){
-// // // //   if (i % 7 == 1){
-// // // //     console.log(i);
-// // // // }
-// // // // }
-
-// // // // for ( let i = 1; i <= 100; i++ ){
-// // // //   if (i % 2 == 0 && i % 5 == 0){
-// // // //     console.log(i);
-// // // //   }
-// // // // }
-
-// // // for (let i = 1; i <= 10; i++) {
-// // //   console.log(i * i);
-// // // }
-
-// // // const ner = "Horlo";
-// // // console.log(ner[4] + ner[3] + ner[2] + ner[1] + ner[0]);
-
-// // // // let start = 1896, end = 2022;
-// // // // for ( let i = start; i <= end; i++ ){
+// // // // let start1 = 1924, end1 = 1994;
+// // // // for ( let i = start1; i = end1; i ++ ){
 // // // //   if (i % 4 == 0){
-// // // //     console.log("The olympic year");
-
-// // // //   } else if (i % 4 == 1){
-// // // //     console.log("Not the olympic year");
+// // // //     console.log(end1);
 
 // // // //   }
 // // // // }
 
-// // // let start1 = 1924, end1 = 1994;
-// // // for ( let i = start1; i = end1; i ++ ){
-// // //   if (i % 4 == 0){
-// // //     console.log(end1);
+// // // // let sar = 12;
+// // // // if ((sar = 12 && sar <= 2)) {
+// // // //   console.log("Uvul");
+// // // // } else if (sar <= 3 && sar >= 5) {
+// // // //   console.log("Havar");
+// // // // } else if (sar <= 6 && sar >= 8) {
+// // // //   console.log("Zun");
+// // // // } else if (sar <= 9 && sar >= 11) {
+// // // //   console.log("Namar");
+// // // // }
 
-// // //   }
+// // // const benzin = 2500,
+// // //   nano = 3000,
+// // //   diesel = 2200;
+// // // let litr = 4;
+// // // let lirt1 = 2;
+// // // let litr2 = 6;
+// // // if (litr) {
+// // //   console.log("92", litr * benzin, "tugrug");
+// // // }
+// // // if (lirt1) {
+// // //   console.log("95", lirt1 * nano, "tugrug");
+// // // }
+// // // if (litr2) {
+// // //   console.log("D", litr2 * diesel, "tugrug");
 // // // }
 
-// // // let sar = 12;
-// // // if ((sar = 12 && sar <= 2)) {
-// // //   console.log("Uvul");
-// // // } else if (sar <= 3 && sar >= 5) {
-// // //   console.log("Havar");
-// // // } else if (sar <= 6 && sar >= 8) {
-// // //   console.log("Zun");
-// // // } else if (sar <= 9 && sar >= 11) {
-// // //   console.log("Namar");
+// // // // const nas = 16;
+// // // // if (nas >= 0 && nas <= 1) {
+// // // //   console.log(nas, "Infant");
+// // // // } else if (nas <= 3 && nas >= 1) {
+// // // //   console.log(nas, "Toddler");
+// // // // } else if (nas <= 5 && nas >= 3) {
+// // // //   console.log(nas, "Preschool");
+// // // // } else if (nas <= 12 && nas >= 5) {
+// // // //   console.log(nas, "Gradeschooler");
+// // // // } else if (nas <= 18 && nas >= 12) {
+// // // //   console.log(nas, "Teen");
+// // // // } else if (nas <= 21 && nas >= 18) {
+// // // //   console.log(nas, "Young Adult");
+// // // // } else if (nas <= 80 && nas >= 21) {
+// // // //   console.log(nas, "Adult");
+// // // // }
+
+// // // const a7 = 153;
+// // // let sum = 0;
+// // // let factorial = a7;
+
+// // // let length = String(a7).length;
+
+// // // for (let i = 0; i < length; i++) {
+// // //   const last = factorial % 10;
+
+// // //   sum += last ** length;
+
+// // //   factorial = parseInt(factorial / 10);
 // // // }
 
-// // const benzin = 2500,
-// //   nano = 3000,
-// //   diesel = 2200;
-// // let litr = 4;
-// // let lirt1 = 2;
-// // let litr2 = 6;
-// // if (litr) {
-// //   console.log("92", litr * benzin, "tugrug");
-// // }
-// // if (lirt1) {
-// //   console.log("95", lirt1 * nano, "tugrug");
-// // }
-// // if (litr2) {
-// //   console.log("D", litr2 * diesel, "tugrug");
-// // }
+// // // console.log(sum === a7);
 
-// // // const nas = 16;
-// // // if (nas >= 0 && nas <= 1) {
-// // //   console.log(nas, "Infant");
-// // // } else if (nas <= 3 && nas >= 1) {
-// // //   console.log(nas, "Toddler");
-// // // } else if (nas <= 5 && nas >= 3) {
-// // //   console.log(nas, "Preschool");
-// // // } else if (nas <= 12 && nas >= 5) {
-// // //   console.log(nas, "Gradeschooler");
-// // // } else if (nas <= 18 && nas >= 12) {
-// // //   console.log(nas, "Teen");
-// // // } else if (nas <= 21 && nas >= 18) {
-// // //   console.log(nas, "Young Adult");
-// // // } else if (nas <= 80 && nas >= 21) {
-// // //   console.log(nas, "Adult");
+// // // let yeo = "*"
+// // // for ( let i = 1; i <= 15; i ++){
+// // // console.log(yeo);
+// // // yeo+="*"
 // // // }
 
-// // const a7 = 153;
-// // let sum = 0;
-// // let temp = a7;
+// // // let soniilber = 0;
+// // // let teniilber = 0;
+// // // for (let i = 1; i <= 100; i++){
+// // //   if (i % 2 === 0)
+// // //     teniilber += i
+// // // if (i % 2 == 1){
+// // //   soniilber += i
+// // // }
+// // // }
+// // //   console.log(soniilber, "sondgoi");
+// // //   console.log(teniilber, "tegsh");
 
-// // let length = String(a7).length;
+// // // WHILE
+// // // let number1 = 1234;
+// // // let sum1 = 0;
 
-// // for (let i = 0; i < length; i++) {
-// //   const last = temp % 10;
+// // // while (number1 > 0) {
+// // //   const last1 = number1 % 10;
+// // //   sum1 += last1;
+// // //   number1 = Math.floor(number1 / 10);
+// // // }
+// // // console.log(sum1);
 
-// //   sum += last ** length;
+// // // let o1 = 54321;
+// // // let sum2 = 0;
 
-// //   temp = parseInt(temp / 10);
+// // // while (o1 < 0){
+// // //   const last2 = o1 % 10;
+// // //   o1 = Math.floor(o1 / 10);
+// // // }
+
+// // // console.log(o1);
+
+// // //  let o2 = "626627";
+// // //  const o3 = o2.length;
+// // //  while (o2 < 0){
+// // //  }
+// // // console.log(o3);
+
+// // let base = 3;
+// // let exp = 4;
+// // let total = 1;
+
+// // while (exp > 0) {
+// //   total *= base;
+// //   exp--;
 // // }
 
-// // console.log(sum === a7);
+// // console.log(total);
 
-// // let yeo = "*"
-// // for ( let i = 1; i <= 15; i ++){
-// // console.log(yeo);
-// // yeo+="*"
+// // let pal = "123218";
+// // let i = 0;
+// // let lenght = pal.lenght;
+// // let isPolindrome = true;
+
+// // while (i < Math.floor(lenght / 2)) {
+// //   let first = pal[i];
+// //   let last3 = pal[lenght - 1 - i];
+
+// //   if (first !== last3) {
+// //     isPolindrome = false;
+// //     break;
+// //   }
+
+// //   i++;
+// // }
+// // console.log(isPolindrome);
+
+// // let g1 = 0,
+// //   g2 = 1;
+// // n = 7,
+// // i = 0;
+// // let next = 0;
+
+// // while (i < n) {
+// //   next = g1 + g2;
+// //   console.log(g1);
+
+// //   g1 = g2;
+// //   g2 = next;
+
+// //   i++;
 // // }
 
-// // let soniilber = 0;
-// // let teniilber = 0;
-// // for (let i = 1; i <= 100; i++){
-// //   if (i % 2 === 0)
-// //     teniilber += i
-// // if (i % 2 == 1){
-// //   soniilber += i
-// // }
-// // }
-// //   console.log(soniilber, "sondgoi");
-// //   console.log(teniilber, "tegsh");
-
-// // WHILE
-// // let number1 = 1234;
-// // let sum1 = 0;
-
-// // while (number1 > 0) {
-// //   const last1 = number1 % 10;
-// //   sum1 += last1;
-// //   number1 = Math.floor(number1 / 10);
-// // }
-// // console.log(sum1);
-
-// // let o1 = 54321;
-// // let sum2 = 0;
-
-// // while (o1 < 0){
-// //   const last2 = o1 % 10;
-// //   o1 = Math.floor(o1 / 10);
-// // }
-
-// // console.log(o1);
-
-// //  let o2 = "626627";
-// //  const o3 = o2.length;
-// //  while (o2 < 0){
-// //  }
-// // console.log(o3);
-
-// let base = 3;
-// let exp = 4;
+// let factorial = 5;
 // let total = 1;
 
-// while (exp > 0) {
-//   total *= base;
-//   exp--;
+// for (let i = 1; i <= factorial; i++) {
+//   total *= i;
 // }
 
-// console.log(total);
+// console.log("For: ", total);
 
-// let pal = "123218";
-// let i = 0;
-// let lenght = pal.lenght;
-// let isPolindrome = true;
+// let whileTotal = 1;
+// let i = 1;
 
-// while (i < Math.floor(lenght / 2)) {
-//   let first = pal[i];
-//   let last3 = pal[lenght - 1 - i];
+// while (i <= factorial) {
+//   whileTotal *= i;
+//   i++;
+// }
 
-//   if (first !== last3) {
-//     isPolindrome = false;
+// console.log("While: ", whileTotal);
+
+// let doWhileTotal = 1;
+// let j = 1;
+
+// do {
+//   doWhileTotal *= j;
+//   j++;
+// } while (j <= factorial);
+
+// console.log("Do While: ", doWhileTotal);
+
+// let random = 0;
+// let h = 2;
+// let k = 1;
+// do {
+//   console.log(Math.floor(Math.random() * 100) + 1);
+//   k++;
+// } while (k < h);
+
+// let input1 = 5;
+// let niilber4 = 0;
+// let l = 1;
+// do {
+//   niilber4 += input1;
+//   l++;
+// } while (l <= input1);
+// console.log(niilber4);
+
+// // let m = 1;
+// // let hurd = 7;
+
+// // do {
+// //   console.log(`${hurd} * ${m} = ${hurd * m}`);
+// //   m++;
+// // } while (m <= 10);
+
+// let password = "horloo";
+// let attempt;
+// let tries = 0;
+
+// Array bodlogo
+
+// let emojis = ["🥩", "🌾", "🥔", "🥕", "🥬"];
+// console.log(emojis.length);
+
+// let urtarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(urtarray);
+
+// let stringarray = ["john", "john", "john", "john", "john"];
+// console.log(stringarray);
+
+// let mass = [1, 2, 3, 4, 5, 6, 7];
+// let lenght2 = mass.lenght;
+// let last4 = mass[lenght2 - 1];
+// console.log(mass[0], mass[lenght2 - 1]);
+
+// let cars = ["Volvo", "BMW", "Ford", "Toyota", "Chevrolet", "Mercedes"];
+// let text = "Ford";
+// for (let i = 0; i < cars.lenght; i++) {
+//   if (cars[i] === text) {
+//     console.log("YES", text);
 //     break;
+//   } else {
+//     console.log("NO", text);
 //   }
-
-//   i++;
-// }
-// console.log(isPolindrome);
-
-// let g1 = 0,
-//   g2 = 1;
-// n = 7,
-// i = 0;
-// let next = 0;
-
-// while (i < n) {
-//   next = g1 + g2;
-//   console.log(g1);
-
-//   g1 = g2;
-//   g2 = next;
-
-//   i++;
 // }
 
-
-let factorial = 5,
-fact = 1,
-niilber3 = 1;
-let yeooo = 1;
-
-for (let i = 1; i <= 5; i ++){
-  // niilber3 = i * i;
+let numberarray = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+let i = numberarray.length;
+for (let i = 0; i < numberarray.length; i++) {
   console.log(i);
-  
-}
-// while (fact <= factorial){
-//   niilber3 = fact * factorial;
-//   fact--;
 
-// }
-// console.log(yeooo);
+  if (numberarray > i) {
+    console.log(i);
+  }
+}
